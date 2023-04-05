c8653d09ad846304155e1ed01834971c1f3959ed
Jaewoo <jaewoocho1017@gmail.com>
2023-04-05 14:16:58 -0700

    added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/home';
+import Login from './pages/login';
+import Profile from './pages/profile';
+import Search from './pages/search';
+import Details from './pages/details';
+
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/*" element={<Home />} />
+          <Route path="/search/*" element={<Search />} />
+          <Route path="/details/*" element={<Details />} />
+          <Route path="/login/*" element={<Login />} />
+          <Route path="/profile/*" element={<Profile />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
