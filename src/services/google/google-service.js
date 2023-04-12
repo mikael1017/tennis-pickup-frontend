72001d79d2a2e0f6161159ed6054c24260df9700
Jaewoo <jaewoocho1017@gmail.com>
2023-04-12 00:36:46 -0700

    removed api key

--- src/services/google/google-service.js
+++ src/services/google/google-service.js
@@ -2,11 +2,10 @@ import axios from "axios";
 const API_BASE = process.env.REACT_APP_API_BASE;
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
-const key = "AIzaSyB0a5eNomoAxP0qzVoG0Zu_bMVzVJTRma0";
 export const getLocationInfo = async (latitude, longitude) => {
 	// this returns null
 	// console.log(GOOGLE_API_KEY);
-	const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${key}`;
+	const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${GOOGLE_API_KEY}`;
 
 	try {
 		const response = await axios.get(url);
